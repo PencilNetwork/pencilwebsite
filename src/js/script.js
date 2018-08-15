9cbaf85e9b8de193e42bb25b3c76abcb2b168902
Mac <mac@Macs-iMac.local>
2018-08-15 17:51:16 +0200

    main page

---
+++ src/js/script.js
@@ -0,0 +1,44 @@
+$(document).ready(function(){
+    
+    $("#pills-home-tab").click(function(){
+        $('#pills-home-tab-img').attr("src","dist/imgs/home/pencil-logo-wt.png");
+        $('#pills-Vision-tab-img').attr("src","dist/imgs/home/vision-gr.png");
+        $('#pills-mission-tab-img').attr("src","dist/imgs/home/mission-gr.png");
+        $('#pills-startup-tab-img').attr("src","dist/imgs/home/startup-gr.png");
+        $('#pills-marketing-tab-img').attr("src","dist/imgs/home/collaborative-marketing-gr.png");
+        
+    }); 
+    $("#pills-Vision-tab").click(function(){
+        $('#pills-home-tab-img').attr("src","dist/imgs/home/pencil-logo-bk.png");
+        $('#pills-Vision-tab-img').attr("src","dist/imgs/home/vision-wt.png");
+        $('#pills-mission-tab-img').attr("src","dist/imgs/home/mission-gr.png");
+        $('#pills-startup-tab-img').attr("src","dist/imgs/home/startup-gr.png");
+        $('#pills-marketing-tab-img').attr("src","dist/imgs/home/collaborative-marketing-gr.png");
+        
+    }); 
+    $("#pills-mission-tab").click(function(){
+        $('#pills-home-tab-img').attr("src","dist/imgs/home/pencil-logo-bk.png");
+        $('#pills-Vision-tab-img').attr("src","dist/imgs/home/vision-gr.png");
+        $('#pills-mission-tab-img').attr("src","dist/imgs/home/mission-wt.png");
+        $('#pills-startup-tab-img').attr("src","dist/imgs/home/startup-gr.png");
+        $('#pills-marketing-tab-img').attr("src","dist/imgs/home/collaborative-marketing-gr.png");
+        
+    }); 
+    $("#pills-startup-tab").click(function(){
+        $('#pills-home-tab-img').attr("src","dist/imgs/home/pencil-logo-bk.png");
+        $('#pills-Vision-tab-img').attr("src","dist/imgs/home/vision-gr.png");
+        $('#pills-mission-tab-img').attr("src","dist/imgs/home/mission-gr.png");
+        $('#pills-startup-tab-img').attr("src","dist/imgs/home/startup-wt.png");
+        $('#pills-marketing-tab-img').attr("src","dist/imgs/home/collaborative-marketing-gr.png");
+        
+    }); 
+    $("#pills-collaborative-marketing-tab").click(function(){
+        $('#pills-home-tab-img').attr("src","dist/imgs/home/pencil-logo-bk.png");
+        $('#pills-Vision-tab-img').attr("src","dist/imgs/home/vision-gr.png");
+        $('#pills-mission-tab-img').attr("src","dist/imgs/home/mission-gr.png");
+        $('#pills-startup-tab-img').attr("src","dist/imgs/home/startup-gr.png");
+        $('#pills-marketing-tab-img').attr("src","dist/imgs/home/collaborative-marketing-wt.png");
+        
+    });
+    
+});
